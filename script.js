f9b367675ffc5fd7de465742b1a1b56847ffe55d
arlea <alexandre.arle@etu.univ-grenoble-alpes.fr>
2022-03-30 09:11:12 +0200

    script js pour requete http

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var access_token = "nrATt8FULnDxKQz-oEbmRxBAM1KG6KirdwXo9yuqDYL6_-07 wXaRtQZj1W6
 
 var search = "PNL";
 
-$.get("https://api.genius.com/search?q=" + search + "&access_token=" + access_token, function(data, status) {
-    alert("Data: " + data + "\nStatus: " + status);
+$("btn-lancer-recherche").click(function() {
+    $.get("https://api.genius.com/search?q=" + search + "&access_token=" + access_token, function(data, status) {
+        alert("Data: " + data + "\nStatus: " + status);
+    });
 });
